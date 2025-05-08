5b34d14aa289d2593a28c311affecb42a9394fdc
yugaljain1999 <yugal.jain1999@gmail.com>
2025-05-08 19:03:55 +0530

    secure DB URL

--- db.py
+++ db.py
@@ -1,11 +1,10 @@
 # Create session local, engine and Declarative base with the help of sqlalchemy - connection between MYsql and fastapi
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL =  "postgresql://neondb_owner:npg_kI5bAlPRp9Qv@ep-quiet-scene-a40sovdq-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"            # 'sqlite:///output.db'  # "mysql+pymysql://root:root9896@127.0.0.1:3306/BlogApp"  
-
-engine = create_engine(url=DATABASE_URL) #connect_args = {"check_same_thread":False})
+engine = create_engine(url=os.environ.get("DATABASE_URL")) #connect_args = {"check_same_thread":False})
 
 # Create session to connect with db, pass some attributes like autocommit, autoflush and bind
 sessionlocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
